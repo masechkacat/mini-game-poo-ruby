fed54b6df6f133ed0cfa7cabf933385e93770a88
masechkacat <masechkacat@gmail.com>
2023-10-16 18:54:49 +0200

    Version 2.0

---
+++ app_2.rb
@@ -0,0 +1,79 @@
+require 'bundler'
+Bundler.require
+
+require_relative 'lib/game'
+require_relative 'lib/player'
+
+puts "---------------------------------------------------"
+puts "|   Bienvenue sur 'ILS VEULENT TOUS MA POO' !     |"
+puts "| Le but du jeu est d'être le dernier survivant ! |"
+puts "---------------------------------------------------"
+
+puts "Rentre ton nickname"
+print ">>>"
+name_of_real_player = gets.chomp
+
+real_player = HumanPlayer.new(name_of_real_player)
+
+player1 = Player.new("Josiane")
+player2 = Player.new("José")
+
+while real_player.life_points > 0 && (player1.life_points > 0 || player2.life_points > 0)
+    puts "\nVoici l'état de nos joueurs :"
+    real_player.show_state
+    player1.show_state
+    player2.show_state
+  
+    puts "\nQuelle action veux-tu effectuer ?"
+    puts "a - Chercher une meilleure arme"
+    puts "s - Chercher à se soigner"
+    puts "attaquer un joueur en vue :"
+    if player1.life_points > 0
+        puts "0 - Josiane a #{player1.life_points} points de vie"
+    end
+
+    if player2.life_points > 0
+        puts "1 - José a #{player2.life_points} points de vie"
+    end
+
+    print ">>>"
+    choice = gets.chomp
+  
+    case choice
+    when "a"
+      real_player.search_weapon
+    when "s"
+      real_player.search_health_pack
+    when "0"
+      if player1.life_points > 0
+          real_player.attacks(player1)
+      else
+          puts "Josiane a déjà été éliminée."
+      end
+    when "1"
+      if player2.life_points > 0
+        real_player.attacks(player2)
+      else
+        puts "José a déjà été éliminé."
+      end
+    end
+  
+    puts "\nLes autres joueurs t'attaquent !"
+    [player1, player2].each do |player|
+      if player.life_points > 0
+        player.attacks(real_player)
+      end
+    end
+  end
+  
+  puts "La partie est finie."
+  
+  if real_player.life_points > 0
+    puts "BRAVO ! TU AS GAGNÉ !"
+  else
+    puts "Loser ! Tu as perdu !"
+  end
+
+
+
+
--- lib/player.rb
+++ lib/player.rb
@@ -1,5 +1,3 @@
-require 'time'
-
 class Player
   attr_accessor :name, :life_points
 
@@ -26,6 +24,52 @@ class Player
     damage = compute_damage
     other_player.gets_damage(damage)
     puts "il lui inflige #{damage} points de dommages"
+  end
+
+ end
+
+ class HumanPlayer < Player
+  attr_accessor :weapon_level
+
+
+  def initialize(name, life_points = 100, weapon_level = 1)
+    @weapon_level = weapon_level
+    super(name, life_points)
+  end
+
+  def show_state
+    puts "#{@name} a #{life_points} points de vie et une arme de niveau #{weapon_level}"
+  end
+
+  def compute_damage
+    super * @weapon_level
+  end
+
+  def search_weapon
+    new_weapon_level = rand(1..6)
+    puts "Tu as trouvé une arme de niveau #{new_weapon_level}"
+
+    if new_weapon_level > @weapon_level
+      @weapon_level = new_weapon_level
+      puts "Youhou ! elle est meilleure que ton arme actuelle : tu la prends."
+    else
+      puts "M@*#$... elle n'est pas mieux que ton arme actuelle..."
+    end
+  end
+
+  def search_health_pack
+    health_pack = rand(1..6)
+
+    case health_pack
+    when 1
+      "Tu n'as rien trouvé... "
+    when 2..5
+      @life_points = (@life_points + 50) > 100 ? @life_points = 100 : @life_points + 50
+      "Bravo, tu as trouvé un pack de +50 points de vie !"
+    when 6
+      @life_points = (@life_points + 80) > 100 ? @life_points = 100 : @life_points + 80
+      "Waow, tu as trouvé un pack de +80 points de vie !"
+    end
   end
 
  end
